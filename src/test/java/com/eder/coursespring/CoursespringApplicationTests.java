5046f6a16cf67b960cf33265435999d685d60f3c
Eder Rabelo <eafonsorabelo@gmail.com>
2025-04-12 16:07:59 -0400

    Project created

---
+++ src/test/java/com/eder/coursespring/CoursespringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eder.coursespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursespringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
