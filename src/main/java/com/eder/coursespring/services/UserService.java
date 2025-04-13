bb734951d05dd660ac4efefa546a6d3a062d1cd4
Eder Rabelo <eafonsorabelo@gmail.com>
2025-04-13 15:30:20 -0400

    Service layer, component registration

---
+++ src/main/java/com/eder/coursespring/services/UserService.java
@@ -0,0 +1,26 @@
+package com.eder.coursespring.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.eder.coursespring.entities.User;
+import com.eder.coursespring.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
