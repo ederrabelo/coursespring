0b32a6fe9b4fd97f0bb785d7c29eae8810042a63
Eder Rabelo <eafonsorabelo@gmail.com>
2025-05-14 22:05:21 -0400

    Exception handling - findById

--- src/main/java/com/eder/coursespring/services/UserService.java
+++ src/main/java/com/eder/coursespring/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.eder.coursespring.entities.User;
 import com.eder.coursespring.repositories.UserRepository;
+import com.eder.coursespring.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
