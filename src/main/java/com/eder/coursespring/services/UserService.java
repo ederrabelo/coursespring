1f55eabf5a74330255817a0932190c48a4139f66
Eder Rabelo <eafonsorabelo@gmail.com>
2025-05-17 10:56:04 -0400

    Exception handling - update

--- src/main/java/com/eder/coursespring/services/UserService.java
+++ src/main/java/com/eder/coursespring/services/UserService.java
@@ -13,6 +13,8 @@ import com.eder.coursespring.repositories.UserRepository;
 import com.eder.coursespring.resources.exceptions.DatabaseException;
 import com.eder.coursespring.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -33,23 +35,27 @@ public class UserService {
 	}
 
 	public void delete(Long id) {
-	    try {
-	        if (repository.existsById(id)) {
-	            repository.deleteById(id);			
-	        } else {				
-	            throw new ResourceNotFoundException(id);			
-	        }		
-	    } catch (EmptyResultDataAccessException e) {			
-	        throw new ResourceNotFoundException(id);		
-	    } catch (DataIntegrityViolationException e) {
-	    	throw new DatabaseException(e.getMessage());
-	    }
+		try {
+			if (repository.existsById(id)) {
+				repository.deleteById(id);
+			} else {
+				throw new ResourceNotFoundException(id);
+			}
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
