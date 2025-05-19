2a8593dc0568f98c5d48fbf5d651ccd14151a5b5
Eder Rabelo <eafonsorabelo@gmail.com>
2025-05-19 17:03:55 -0400

    Implements database connection

--- src/main/java/com/eder/coursespring/config/TestConfig.java
+++ src/main/java/com/eder/coursespring/config/TestConfig.java
@@ -22,7 +22,7 @@ import com.eder.coursespring.repositories.ProductRepository;
 import com.eder.coursespring.repositories.UserRepository;
 
 @Configuration
-@Profile("test")
+@Profile({"test", "local"})
 public class TestConfig implements CommandLineRunner {
 
 	@Autowired
